4878a9ae4df83fe5cf2ff8de30f45b0d85ce3f6d
Babalola Muhammad <barmilitary@yahoo.com>
2023-02-09 09:22:33 +0100

    changes made

--- api/controllers/booking.js
+++ api/controllers/booking.js
@@ -176,7 +176,7 @@ export const getIncome = async (req, res, next) => {
               "count": { "$sum": 1 },
             },
           },
-        ]).sort({ "_id.year": -1, "_id.year": -1 });
+        ]).sort({ "_id.year": -1, "_id.month": -1 });
         res.status(200).json(income);
       } catch (err) {
         res.status(500).json(err);
